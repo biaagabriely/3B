5ee62a251375acd3be365d05a5eb240530c2dcb8
biaagabriely <127507324+biaagabriely@users.noreply.github.com>
2024-03-15 11:19:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const botoes= querySelectorAll(".botao");
-console.log(botoes);
+
+for(let i= 0;<botoes.length; i++) {
+    botoes [i].onclick = function (){
+        for (j=; j < botoes.lenght; j++){
+            botoes[j].remove ("ativo");
+
+        }
+        botoes[j].classlist.add("ativo")
+    }
+
+}
+        botoes 
